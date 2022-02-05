a31bc94e23367c646137184d9159d710e9082304
khalil-sudo <khalilguezguez30@gmail.com>
2022-02-06 00:35:57 +0100

    just run your npm i and boom

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface Coin {
+  id: string;
+  name: string;
+  symbol: string;
+  image: string;
+  current_price: number;
+  price_change_percentage_24h: number;
+  total_volume: number;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'angular-coin-api';
+export class AppComponent implements OnInit {
+  coins: Coin[] = [];
+  titles: string[] = [
+    '#',
+    'Coin',
+    'Prix',
+    'Changement',
+    'Volume en circul. 24h',
+  ];
+  searchText = '';
+  filteredCoins: Coin[] = [];
+
+  constructor(private http: HttpClient) {}
+  searchCoin() {
+    this.filteredCoins = this.coins.filter(
+      (coin) =>
+        coin.name
+          .toLocaleLowerCase()
+          .includes(this.searchText.toLocaleLowerCase()) ||
+        coin.symbol
+          .toLocaleLowerCase()
+          .includes(this.searchText.toLocaleLowerCase())
+    );
+  }
+
+  ngOnInit() {
+    this.http
+      .get<Coin[]>(
+        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'
+      )
+      .subscribe(
+        (res) => {
+          console.log(res);
+          this.coins = res;
+          this.filteredCoins = res;
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
+  }
 }
